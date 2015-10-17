cfe738be55f1f1a4418ca89ca5ee8ca62d5af01c
Clark159 <clark159@hotmail.com>
2015-10-17 23:43:09 +0800

    Display menu item by permissions

---
+++ nuget/push.bat
@@ -0,0 +1,4 @@
+NuGet.exe push CLK.AspNet.Identity.Mvc.Template.2.2.2.nupkg
+NuGet.exe push CLK.AspNet.Identity.Mvc.2.2.2.nupkg
+NuGet.exe push CLK.AspNet.Identity.EntityFramework.2.2.2.nupkg
+NuGet.exe push CLK.AspNet.Identity.2.2.2.nupkg
